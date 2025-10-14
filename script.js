d7a0c5e1c9931be65ddf02701969a5166ba029ea
Daniil6474989 <112860427+Daniil6474989@users.noreply.github.com>
2025-10-14 17:49:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const questionHeaders = document.querySelectorAll('.process-question');
+
+  questionHeaders.forEach(function (header) {
+    header.addEventListener('click', function (e) {
+
+      const processItem = this.closest('.process-item');
+      if (processItem) {
+        processItem.classList.toggle('active');
+      }
+    });
+  });
+});
+
+
+    const swiper = new Swiper('.mySwiper', {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      mousewheel: true,
+      grabCursor: true,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      on: {
+        click(e) {
+          const clickedSlide = e.target.closest('.swiper-slide');
+          if (clickedSlide) {
+            const slideIndex = parseInt(clickedSlide.getAttribute('data-swiper-slide-index'), 10);
+            if (!isNaN(slideIndex)) {
+              swiper.slideTo(slideIndex);
+            }
+          }
+        }
+      },
+
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1920: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        }
+      }
+    });
